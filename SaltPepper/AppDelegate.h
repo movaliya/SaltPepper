2dd6f139d85937ca8141a898db6ec6eaa44a0f2e
kaushik <kaushik@kaushiks-MacBook-Pro.local>
2018-03-11 13:23:49 +0530

    initial comment

---
+++ SaltPepper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaltPepper
+//
+//  Created by kaushik on 11/03/18.
+//  Copyright © 2018 kaushik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SaltPepper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SaltPepper
+//
+//  Created by kaushik on 11/03/18.
+//  Copyright © 2018 kaushik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
