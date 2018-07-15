d43193f4aeb41ce8016285aa9a6cec45f7d43538
jignesh solanki <jigneshbs@outlook.com>
2018-07-15 18:23:48 +0530

    some bugs solve

--- SaltPepper/RegisterVW.m
+++ SaltPepper/RegisterVW.m
@@ -13,7 +13,7 @@
 #import "saltPepper.pch"
 
 @interface RegisterVW ()
--(void)handleFBSessionStateChangeWithNotification:(NSNotification *)notification;
+//-(void)handleFBSessionStateChangeWithNotification:(NSNotification *)notification;
 @property AppDelegate *appDelegate;
 @end
 
@@ -39,10 +39,10 @@
     signin.delegate = self;
     signin.uiDelegate = self;
     
-    [[NSNotificationCenter defaultCenter] addObserver:self
+    /*[[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(handleFBSessionStateChangeWithNotification:)
                                                  name:@"SessionStateChangeNotification"
-                                               object:nil];
+                                               object:nil];*/
     
     
 }
@@ -184,76 +184,7 @@
     }];
     
 }
-// Login After Regiter Complete
--(void)CallForloging :(NSString *)EmailStr Password:(NSString *)PasswordStr
-{
-    
-    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    NSMutableDictionary *dict1 = [[NSMutableDictionary alloc] init];
-    
-    [dict1 setValue:KAPIKEY forKey:@"APIKEY"];
-    
-    NSMutableDictionary *dictInner = [[NSMutableDictionary alloc] init];
-    
-    [dictInner setObject:EmailStr forKey:@"EMAIL"];
-    [dictInner setObject:PasswordStr forKey:@"PASSWORD"];
-    [dictInner setObject:@"firebase" forKey:@"REGID"];
-    
-    NSMutableDictionary *dictSub = [[NSMutableDictionary alloc] init];
-    
-    [dictSub setObject:@"action" forKey:@"MODULE"];
-    
-    [dictSub setObject:@"authenticate" forKey:@"METHOD"];
-    
-    [dictSub setObject:dictInner forKey:@"PARAMS"];
-    
-    NSMutableArray *arr = [[NSMutableArray alloc] initWithObjects:dictSub, nil];
-    NSMutableDictionary *dictREQUESTPARAM = [[NSMutableDictionary alloc] init];
-    
-    [dictREQUESTPARAM setObject:arr forKey:@"REQUESTPARAM"];
-    [dictREQUESTPARAM setObject:dict1 forKey:@"RESTAURANT"];
-    
-    NSError* error = nil;
-    
-    NSString *makeURL=[NSString stringWithFormat:@"%@%@",kBaseURL,LOGINKEY];
-    
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictREQUESTPARAM options:NSJSONWritingPrettyPrinted error:&error];
-    // NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData
-                                                         options:NSJSONReadingMutableContainers
-                                                           error:&error];
-    
-    [Utility postRequest:json url:makeURL success:^(id responseObject) {
-        
-        [MBProgressHUD hideHUDForView:self.view animated:YES];
-        
-        NSLog(@"responseObject==%@",responseObject);
-        NSString *SUCCESS=[[[[responseObject objectForKey:@"RESPONSE"] objectForKey:@"action"] objectForKey:@"authenticate"] objectForKey:@"SUCCESS"];
-        if ([SUCCESS boolValue] ==YES)
-        {
-            [AppDelegate WriteData:@"LoginUserDic" RootObject:responseObject];
-            _wb(@"isSkip", YES);
-            Email_TXT.text=@"";
-            Password_TXT.text=@"";
-            DEMORootViewController *vcr = [[UIStoryboard storyboardWithName:[SharedClass sharedSingleton].storyBaordName  bundle:nil] instantiateViewControllerWithIdentifier:@"rootController"];
-            [self.navigationController pushViewController:vcr animated:YES];
-             [AppDelegate showErrorMessageWithTitle:@"" message:@"Registration successful" delegate:nil];
-            //[AppDelegate showErrorMessageWithTitle:@"" message:@"Login successful" delegate:nil];
-        }
-        else
-        {
-            [AppDelegate showErrorMessageWithTitle:@"" message:@"Email and/or Password did not matched." delegate:nil];
-        }
-        
-    } failure:^(NSError *error) {
-        
-        [MBProgressHUD hideHUDForView:self.view animated:YES];
-        
-        NSLog(@"Fail");
-    }];
-    
-   
-}
+
 - (IBAction)ShowPasswordBtn_Click:(id)sender
 {
     if (Password_TXT.secureTextEntry==YES)
@@ -300,7 +231,7 @@
 }
 
 #pragma mark - Private method implementation
-
+/*
 -(void)handleFBSessionStateChangeWithNotification:(NSNotification *)notification
 {
     NSLog(@"result");
@@ -355,7 +286,7 @@
         // In case an error has occurred, then just log the error and update the UI accordingly.
         NSLog(@"Error: %@", [error localizedDescription]);
     }
-}
+}*/
 -(void)FBPostData
 {
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
