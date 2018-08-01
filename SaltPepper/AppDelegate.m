0b07d6dcb51ea92fc0a9da36e8193f2bdde973c2
jigneshbs <jigneshbs@outlook.com>
2018-08-01 11:18:18 +0530

    just check

--- SaltPepper/AppDelegate.m
+++ SaltPepper/AppDelegate.m
@@ -24,6 +24,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // comment
     if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad)
     {
         [SharedClass sharedSingleton].storyBaordName = @"MainiPad";
