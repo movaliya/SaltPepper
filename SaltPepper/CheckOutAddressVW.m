c9ad9d96ff48a0ddf84eb9fccb425641185485e6
Dharmraj412 <dharmraj41@gmail.com>
2018-08-01 23:50:19 +0530

    point 8 also complete.

--- SaltPepper/CheckOutAddressVW.m
+++ SaltPepper/CheckOutAddressVW.m
@@ -81,8 +81,8 @@
     [datepicke1 addTarget:self action:@selector(DeliveyTimeTextField:) forControlEvents:UIControlEventValueChanged];
     [self.DeliveryTimeTXT setInputView:datepicke1];
     
-//    CollectionTime_LBL.text = [self formatDate:[NSDate date]];
-//    DeliveryTime_LBL.text = [self formatDate:[NSDate date]];
+    _CollectionTime_LBL.text = [self formatDate:[NSDate date]];
+    _DeliveryTime_LBL.text = [self formatDate:[NSDate date]];
     
     BOOL internet=[AppDelegate connectedToNetwork];
     if (internet)
